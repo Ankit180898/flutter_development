08b3bc2c50446a10ece6ecc8754bdf9214d00c5b
Ankit Kumar <karan180898@gmail.com>
2022-04-05 10:47:39 +0530

    Chapter 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter_development/pages/home_page.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "Awesome app",
-    home: HomePage(),
-  ));
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Awesome App"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Align(
-          alignment: Alignment.centerLeft,
-          child: Container(
-            color: Colors.black,
-            width: 200,
-            height: 400,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.green,
-                ),
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.yellow,
-                ),
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.red,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
+  runApp(
+    MaterialApp(
+      title: "Awesome app",
+      home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.purple),
+    ),
+  );
 }
