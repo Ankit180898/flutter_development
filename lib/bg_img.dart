17269a80d27c428025b96e6fdbc4123c076cbe09
Ankit Kumar <karan180898@gmail.com>
2022-04-05 16:19:41 +0530

    Chapter 4 completed

--- lib/bg_img.dart
+++ lib/bg_img.dart
@@ -7,6 +7,8 @@ class bgImg extends StatelessWidget {
     return Image.asset(
       "assets/ankit.jpg",
       fit: BoxFit.cover,
+      color: Colors.black.withOpacity(0.7),
+      colorBlendMode: BlendMode.darken,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_development/pages/home_page.dart';
+import 'package:flutter_development/pages/login_page.dart';
 
 void main() {
   runApp(
     MaterialApp(
       title: "Awesome app",
-      home: HomePage(),
-      theme: ThemeData(primarySwatch: Colors.purple),
+      home: LoginPage(),
+      theme: ThemeData(primarySwatch: Colors.purple,
+    ),
+    routes: {
+      "/login": (context) => LoginPage(),
+      "/home": (context) => HomePage()
+    } ,
     ),
   );
 }
